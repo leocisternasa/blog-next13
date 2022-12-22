036211e95df64089c3de0eef416a16a18d0bfa2f
leocisternasa <leocisal@gmail.com>
2022-12-22 13:27:20 -0300

    reading posts folder, getting slugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import fs from "fs";
+import { get } from "http";
+
+const getPostMetadata = () => {
+    const folder = "posts/";
+    const files = fs.readdirSync(folder);
+    const markdownPosts = files.filter((file)=> file.endsWith(".md"));
+    const slugs = markdownPosts.map((file)=> file.replace(".md", ""));
+    return slugs;
+};
+
+
+
 const HomePage = () => {
-    return <h1>Hello world</h1>
+    const postMetadata = getPostMetadata();
+    const postPreviews = postMetadata.map((slug)=>(
+        <div>
+            <h2>{slug}</h2>
+        </div>
+    ))
+    return <h1>{postPreviews}</h1>
 }
 
 export default HomePage
