f2a1d5bb6ad971848a553ac9267143aa75070d75
leocisternasa <leocisal@gmail.com>
2022-12-22 13:34:52 -0300

    adding prettierrs.json

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,22 @@
-import fs from "fs";
-import { get } from "http";
+import fs from 'fs'
+import { get } from 'http'
 
 const getPostMetadata = () => {
-    const folder = "posts/";
-    const files = fs.readdirSync(folder);
-    const markdownPosts = files.filter((file)=> file.endsWith(".md"));
-    const slugs = markdownPosts.map((file)=> file.replace(".md", ""));
-    return slugs;
-};
-
-
+  const folder = 'posts/'
+  const files = fs.readdirSync(folder)
+  const markdownPosts = files.filter(file => file.endsWith('.md'))
+  const slugs = markdownPosts.map(file => file.replace('.md', ''))
+  return slugs
+}
 
 const HomePage = () => {
-    const postMetadata = getPostMetadata();
-    const postPreviews = postMetadata.map((slug)=>(
-        <div>
-            <h2>{slug}</h2>
-        </div>
-    ))
-    return <h1>{postPreviews}</h1>
+  const postMetadata = getPostMetadata()
+  const postPreviews = postMetadata.map(slug => (
+    <div>
+      <h2>{slug}</h2>
+    </div>
+  ))
+  return <h1>{postPreviews}</h1>
 }
 
 export default HomePage
